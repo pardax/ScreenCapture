7219744b01cad75d964b5444eefa01c46bff629d
pardax <get_ant@naver.com>
2022-11-30 11:22:06 +0900

    초기 커밋.

---
+++ Capture/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbScreen = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(69, 20);
+            this.toolStripStatusLabel1.Text = "화면캡쳐";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(84, 20);
+            this.toolStripStatusLabel2.Text = "화면지우기";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(69, 20);
+            this.toolStripStatusLabel3.Text = "캡쳐저장";
+            // 
+            // pbScreen
+            // 
+            this.pbScreen.BackColor = System.Drawing.Color.Silver;
+            this.pbScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbScreen.Location = new System.Drawing.Point(0, 0);
+            this.pbScreen.Name = "pbScreen";
+            this.pbScreen.Size = new System.Drawing.Size(800, 424);
+            this.pbScreen.TabIndex = 1;
+            this.pbScreen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbScreen);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.PictureBox pbScreen;
+    }
+}
+
---
+++ Capture/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+
+namespace Capture
+{
+    public partial class Form1 : Form
+    {
+        Point oriLocalPoint;
+        Size oriLocalSize;
+
+        bool original = true;
+        bool isCaptured = false;
+
+        Graphics screenG;
+        Bitmap capWindow;
+
+        System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 'c')
+            {
+                original = false;
+                isCaptured = true;
+
+                this.Opacity = 0.0;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.Location = new Point(0, 0);
+                this.Size = Screen.PrimaryScreen.Bounds.Size;
+
+                var fullScreen = Screen.PrimaryScreen.Bounds;
+
+                capWindow = new Bitmap(fullScreen.Width, fullScreen.Height);
+
+                screenG = Graphics.FromImage(capWindow);
+
+                screenG.CopyFromScreen(PointToScreen(new Point(0, 0)), new Point(0, 0), fullScreen.Size);
+
+                pbScreen.Image = capWindow;
+
+                soundPlayer.SoundLocation = @"..\..\wav\capture.wav";
+                soundPlayer.Play();
+
+                this.Opacity = 1.0;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.Location = oriLocalPoint;
+                this.Size = oriLocalSize;
+                isCaptured = true;
+            }
+            else if(e.KeyChar == 'e')
+            {
+                soundPlayer.SoundLocation = @"..\..\wav\eraser.wav";
+                soundPlayer.Play();
+
+                isCaptured = false;
+                pbScreen.Image = null;
+
+                
+            }
+            else if(e.KeyChar == 's')
+            {
+                if (isCaptured)
+                {
+                    using (var Sfile = new SaveFileDialog())
+                    {
+                        Sfile.OverwritePrompt = true;
+                        Sfile.FileName = "화면캡쳐";
+                        Sfile.Filter = "이미지파일(*.jpg) | *.jpg";
+                        DialogResult result = Sfile.ShowDialog();
+
+                        if(result == DialogResult.OK)
+                            capWindow.Save(Sfile.FileName, ImageFormat.Jpeg);       
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("캡쳐한 화면이 없습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            if (isCaptured)
+            {
+                oriLocalPoint = Location;
+                oriLocalSize = Size;
+            }
+        }
+    }
+}
